14681106ba168bea8cfad76c7a4ef8329462c691
Aparna Goyal <aparnagoyal.0003@gmail.com>
2022-09-21 06:33:02 +0530

    deployed the react app using github pages

---
+++ build/precache-manifest.56a25673407b03b36b7034a794c87408.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b27002865d736c17ae63926f8fca10",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "be8c9388e0c2bb835496",
+    "url": "/Emojipedia/static/js/2.bc581622.chunk.js"
+  },
+  {
+    "revision": "cc09b2d7671dc54e4eab",
+    "url": "/Emojipedia/static/js/main.903435b6.chunk.js"
+  },
+  {
+    "revision": "8e45ecf4ae811e339f7c",
+    "url": "/Emojipedia/static/js/runtime-main.ccd8d89c.js"
+  }
+]);
